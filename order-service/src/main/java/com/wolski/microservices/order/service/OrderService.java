8a81012e67b23387edd459b21cc841354cc6987e
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-10-08 11:09:15 +0200

    Added communication between Order service and Inventory service

--- order-service/src/main/java/com/wolski/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/wolski/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.wolski.microservices.order.service;
 
+import com.wolski.microservices.order.client.InventoryClient;
 import com.wolski.microservices.order.dto.OrderRequest;
 import com.wolski.microservices.order.model.Order;
 import com.wolski.microservices.order.repository.OrderRepository;
@@ -13,15 +14,21 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
+        if(isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
 
-        orderRepository.save(order);
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
+        }
     }
 }
