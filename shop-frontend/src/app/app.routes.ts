bdc0ca683c255f65b1267be25890f0bc53af3ef0
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-10-16 11:19:41 +0200

    Added routes

--- shop-frontend/src/app/app.routes.ts
+++ shop-frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomePageComponent} from "./components/home-page/home-page.component";
+import {AddProductComponent} from "./components/add-product/add-product.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'add-product', component: AddProductComponent}
+];
