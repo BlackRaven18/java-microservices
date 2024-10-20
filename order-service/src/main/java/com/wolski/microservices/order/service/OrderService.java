2604e7f462372f906caa8c11fcec62366467df48
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-10-20 22:30:06 +0200

    Added new Kafka event in Order service

--- order-service/src/main/java/com/wolski/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/wolski/microservices/order/service/OrderService.java
@@ -2,19 +2,24 @@ package com.wolski.microservices.order.service;
 
 import com.wolski.microservices.order.client.InventoryClient;
 import com.wolski.microservices.order.dto.OrderRequest;
+import com.wolski.microservices.order.event.OrderPlacedEvent;
 import com.wolski.microservices.order.model.Order;
 import com.wolski.microservices.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
         var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
@@ -25,8 +30,12 @@ public class OrderService {
             order.setPrice(orderRequest.price());
             order.setSkuCode(orderRequest.skuCode());
             order.setQuantity(orderRequest.quantity());
-
             orderRepository.save(order);
+
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+            log.info("Start - Sending OrderPlacedEvent: {} to Kafka topic order-placed", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End - Sending OrderPlacedEvent: {} to Kafka topic order-placed", orderPlacedEvent);
         } else {
             throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
         }
